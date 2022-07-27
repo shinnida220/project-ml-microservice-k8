7177ec089b3dc5452a5b191d54945c8d913ec86f
Cloud9 <ibk.com@gmail.com>
2022-07-27 00:43:32 +0000

    updated files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="nuelibk/python-prediction"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run prediction-app --image=$dockerpath --port=80
+kubectl create deploy python-prediction --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl wait --for=condition=ready pods/prediction-app
+kubectl get po -A
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward prediction-app --address 0.0.0.0 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="nuelibk/python-prediction"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag python-prediction $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push "$dockerpath"
